77b66719a90a0d19ffab91ba0d712fd4ed9966c4
Snipez9492 <waleibk92@gmail.com>
2021-06-01 21:36:25 -0400

    Fahrenheit to Celsius

--- main.py
+++ main.py
@@ -2,4 +2,14 @@
 Write a program that converts temperature in different countries.
 """
 
-print("This is the first commit.")
+# print("This is the first commit.")
+
+
+def fahrenheit_to_celsius(degree):
+    ans = ((degree - 32) * 5 / 9)
+    return ans
+
+
+response = int(input("What is the degrees in fahrenheit: "))
+
+print("In celsius it will convert to " + str(fahrenheit_to_celsius(response)))
